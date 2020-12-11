ed05e06f90bfe1f075a8a44e764402e55440d108
erik <erik>
2020-12-11 20:49:19 +0100

    prevent favicon load

--- dashboard/index.js
+++ dashboard/index.js
@@ -120,6 +120,7 @@ function sendResponse(res, html) {
 					<meta charset="UTF-8">
 					<meta name="viewport" content="width=device-width, initial-scale=1.0">
 					<title>Dashboard</title>
+					<link rel="icon" href="data:,">
 				
 					<!--Import Google Icon Font-->
 					<link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
