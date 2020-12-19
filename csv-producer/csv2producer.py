87644ad94b0dfc21427b7d303c6211b81f75ca31
Manuk96 <manuk96@gmx.de>
2020-12-19 22:40:24 +0100

    fixed some producer errors

--- csv-producer/csv2producer.py
+++ csv-producer/csv2producer.py
@@ -3,6 +3,8 @@ from json import dumps
 from kafka import KafkaProducer
 from random import random
 import csv
+import os
+import sys
 
 dbOptions = {"host": "my-app-mysql-service", 'port': 33060, "user": "root", "password": "mysecretpw"}
 kafka_broker = "my-cluster-kafka-bootstrap:9092"
@@ -19,32 +21,42 @@ def randomClientId():
 # Maybe add batching????
 
 def getProducer():
-    kafka_producer = None
-    try:
-        kafka_producer = KafkaProducer(bootstrap_servers=kafka_broker,
-                                 client_id = randomClientId(),
-                                 value_serializer = lambda x :
-                                 dumps(x).encode("utf-8"),
-                                 retries=2)
-    except Exception as ex:
-        console.log("Kafka-Connection failed")
-        console.log(str(ex))
-    finally:
-        return kafka_producer
+    kafka_producer = KafkaProducer(bootstrap_servers = [kafka_broker],
+                                  client_id = randomClientId(),
+                                  value_serializer = lambda x :
+                                  dumps(x).encode("utf-8"),
+                                  retries=10,
+                                  api_version=(0, 10, 1))
+    return kafka_producer
 
-def send_message(data):
-    try:
-        getProducer.send(kafka_topic, value=data)
-        console.log("Sent to producer")
-    except Exception as ex:
-        console.log("Failed to send to producer")
-        print(str(ex))
+# def getProducers():
+#     kafka_producer = None
+#     try:
+#         kafka_producer = KafkaProducer(bootstrap_servers=kafka_broker,
+#                                        client_id = randomClientId(),
+#                                        value_serializer = lambda x : dumps(x).encode("utf-8"),
+#                                        retries=10,
+#                                        api_version=(0, 10, 1))
+#     except Exception as ex:
+#         console.log("Kafka-Connection failed")
+#         console.log(str(ex))
+#     finally:
+#         return kafka_producer
 
+def send_message(producer, data):
+    while not producer.bootstrap_connected():
+        sleep(1)
+
+    producer.send(kafka_topic, value=data)
+    # console.log("Sent to producer")
+    # except Exception as ex:
+    #     console.log("Failed to send to producer")
+    #     print(str(ex))
+    
 # read in csv-File
 
-producer = getProducer()
-
-with open(os.path.join("app", "test.csv"), "rU") as file:
+with open(os.path.join(sys.path[0], "test.csv"), "r", encoding="utf8") as file:
+    producer = getProducer()
     for line in file:
         if counter > 0:
             lines = line.split(";")
@@ -53,12 +65,12 @@ with open(os.path.join("app", "test.csv"), "rU") as file:
                     'n_serie' : lines[5],
                     'n_season' : lines[6],
                     'sentence' : lines[3]}
-            producer.send(kafka_topic, value=data)
+            send_message(producer, data)
 
             if ((counter > 1) & (counter % 20 == 0)):
                 sleep(5)
 
         counter += 1
     
-    if producer is not None:
-        producer.close()
+#    if producer is not None:
+#        producer.close()
